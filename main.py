c04d70b05b2f5ef305f48a13875ea92d650627ce
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-07-27 23:40:55 +0200

    import routers

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from app.routers import users
+from app.routers import items
+
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items")
-def read_items():
-    return {
-        "status": True,
-        "data": []
-    }
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+app.include_router(users.router)
+app.include_router(items.router)
